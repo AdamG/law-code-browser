ac9824008977346ccec3adefaf4b1918a902e5cb
Adam Gomaa <adam@adam.gomaa.us>
2009-03-30 16:40:16 -0500

    Add an index view.

--- law_code_browser/urls.py
+++ law_code_browser/urls.py
@@ -10,16 +10,27 @@ admin.autodiscover()
 
 import os
 
-urlpatterns = patterns('',
-    url(r'^$', direct_to_template, {"template": "homepage.html"}, name="home"),
-    
+from law_code.models import Code
+
+def index(request):
+    code_list = Code.objects.filter(public=True).order_by("name")
+    return direct_to_template(
+        request, 'law_code/code_list.html',
+        {"code_list": code_list})
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', index, {}, name="home"),
+    (r'^law/', include('law_code.urls')),
+
     (r'^about/', include('about.urls')),
     (r'^account/', include('account.urls')),
     (r'^openid/(.*)', PinaxConsumer()),
     (r'^profiles/', include('basic_profiles.urls')),
     (r'^notices/', include('notification.urls')),
     (r'^announcements/', include('announcements.urls')),
-    
+
     (r'^admin/(.*)', admin.site.root),
 )
 
